ac641406436918da030714e0d8bef5fe657fbdc8
Irfan Chairurrachman <irfanchairurrachman@gmail.com>
2023-11-29 15:35:25 +0700

    doc: add code explanation for streamlit

--- module4/streamlit/simple.py
+++ module4/streamlit/simple.py
@@ -7,17 +7,25 @@ import streamlit as st
 
 load_dotenv()
 
+# initialize llm
 llm = OpenAI(temperature=0, streaming=True)
 
+# intialize DuckDuckGo search tools
 tools = load_tools(['ddg-search'])
+# creating agent that connect llm to tools
 agent = initialize_agent(
     tools=tools, llm = llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION
 )
 
+# Create input, if input provided then assign to prompt
 if prompt := st.chat_input():
+    # display user question and assistant icon
     st.chat_message("user").write(prompt)
     with st.chat_message("assistant"):
         st.write("🧠 Thinking..")
+        # create StreamlitCallbackHandler as container
         st_callback = StreamlitCallbackHandler(st.container())
+        # run agent to answer the prompt
         response = agent.run(prompt, callbacks = [st_callback])
+        # write response
         st.write(response)
